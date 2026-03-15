bc9eb1c1c52bd7647d2326301c5f8585a872e492
vishal6626 <78248613+vishal6626@users.noreply.github.com>
2026-03-15 13:09:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -167,5 +167,6 @@ def do_delete_report():
         return jsonify({'success': True})
     return jsonify({'error': 'Failed to delete report.'}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
